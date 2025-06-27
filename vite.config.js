199ee4c885a2edde8df1e27aeec7ab21f3406e72
doyindevops <adedoyindevops@gmail.com>
2025-06-28 00:44:03 +0100

    vitr.config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/adedoyin-portfolio/',  // 👈 important
   plugins: [react()],
-});
+  base: '/adedoyin-portfolio/' // must match your GitHub repo name exactly
+})
