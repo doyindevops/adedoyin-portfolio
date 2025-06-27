05697843f1fd984cbaeef391eaa47e2658c34864
doyindevops <adedoyindevops@gmail.com>
2025-06-28 00:33:52 +0100

    Prepare for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/adedoyin-portfolio/',  // 👈 important
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 });
-
