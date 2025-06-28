c6b5060c6727420e96ccbb53df8eb9a9f5a0f468
doyindevops <adedoyindevops@gmail.com>
2025-06-28 02:15:25 +0100

    Remove missing index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // Optional
+// import './index.css'  // Optional
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
