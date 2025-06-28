c5e1cb99111ce8b7d5cdf1c0860e2abd4144fefd
doyindevops <adedoyindevops@gmail.com>
2025-06-28 01:29:23 +0100

    Cleaned up unnecessary files for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/adedoyin-portfolio/', // this must match the repo name
-});
+  plugins: [react()]
+})
